66ac592767c73c6448f17f5f448939463ae466a0
sergey <smchonin@gmail.com>
2015-03-07 02:57:03 +0400

    added File class for reflecting file storage contents

--- db/models.py
+++ db/models.py
@@ -1,6 +1,6 @@
 __author__ = 'sergey'
 
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.engine.url import URL
 from sqlalchemy.ext.declarative import declarative_base
 import db.settings as settings
@@ -19,11 +19,25 @@ def connect():
     """
     return create_engine(URL(**settings.DATABASE))
 
+
 def create_file_table(engine):
     """"""
     DeclarativeBase.metadata.create_all(engine)
+
 
 def create_schedule_table(engine):
     """"""
     DeclarativeBase.metadata.create_all(engine)
 
+
+class File(DeclarativeBase):
+    """
+    File table reflects files in file storage
+    """
+    __tablename__ = 'files'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    time_length = Column(Integer)  # in seconds
+
+    def __repr__(self):
+        return "<File(name='%s', time_length='%s')>" % (self.name, self.time_length)
