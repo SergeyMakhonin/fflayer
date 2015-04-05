c1dd67701408efeba166eb377539cf821a305e2f
sergey <smchonin@gmail.com>
2015-04-05 20:53:06 +0300

    added DataBase class to work with databases according to settings.py)

--- db/processing.py
+++ db/processing.py
@@ -7,30 +7,40 @@ import db.settings as settings
 from db.models import *
 
 
-# Function used to create session object @ create_session_object()
-def connect():
-    """
-    Performs database connection using database settings from settings.py.
-    Returns sqlalchemy engine instance
-    """
-    return create_engine(URL(**settings.DATABASE))
+class DataBase(object):
 
+    def __init__(self):
+        self.connection_made = False
+        self.session = None
 
-# Create DB connection object & associate it with a Session class to make a session
-def create_session_object():
-    """
-    Returns session = Session() object that is used for DB transactions
-    :return: Session
-    """
-    engine = connect()
-    Session = sessionmaker(bind=engine)
-    return Session()
+    def connect(self):
+        """
+        Performs database connection using database settings from settings.py.
+        Returns sqlalchemy engine instance
+        Method used to create session object @ create_session_object()
+        """
+        self.connection_made = True
+        return create_engine(URL(**settings.DATABASE))
 
+    def create_session_object(self):
+        """
+        Create DB connection object & associate it with a Session class to make a session
+        Session() object is used for DB transactions
+        """
+        engine = self.connect()
+        Session = sessionmaker(bind=engine)
+        self.session = Session()
 
-# Create tables
-def create_tables():
-    engine = connect()
-    create_schemma(engine)
+    def create_tables(self):
+        """
+        Create tables (use if no relations exists)
+        Does not need a Session()
+        """
+        engine = self.connect()
+        create_schemma(engine)
+
+    def add_object(self, ObjectInstance):
+        self.session.add(ObjectInstance)
 
 # session = create_session_object()
 # pdb.set_trace()
